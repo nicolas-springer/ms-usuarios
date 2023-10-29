0e7e619dda20302989474cf8ddc0bb5297609e07
NicoBriani <brianinicolas11@gmail.com>
2023-10-29 16:31:39 -0300

    actualizacion 1

--- src/main/java/dan/ms/tp/msusuarios/service/ClienteServiceImpl.java
+++ src/main/java/dan/ms/tp/msusuarios/service/ClienteServiceImpl.java
@@ -1,59 +1,73 @@
 package dan.ms.tp.msusuarios.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dan.ms.tp.msusuarios.dao.ClienteJpaRepository;
 import dan.ms.tp.msusuarios.modelo.Cliente;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService{
 
+    @Autowired
+    private ClienteJpaRepository clienteJpaRepository;
+
     @Override
     public List<Cliente> getAllClientes() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllClientes'");
+        return clienteJpaRepository.findAll();
+        
     }
 
-    @Override
+   @Override
     public Cliente getClienteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCliente'");
+        Optional<Cliente> optionalCliente = clienteJpaRepository.findById(id);
+        return optionalCliente.orElse(null);
     }
 
     @Override
     public Cliente getClienteByCuit(String cuit) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCliente'");
+        return clienteJpaRepository.findByCuit(cuit).orElse(null);
     }
 
     @Override
     public Cliente createCliente(Cliente cliente) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createCliente'");
+        return clienteJpaRepository.save(cliente);
     }
 
     @Override
     public Cliente updateClienteById(Integer id, Cliente cliente) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateClienteById'");
+        if (clienteJpaRepository.existsById(id)) {
+            cliente.setId(id);
+            return clienteJpaRepository.save(cliente);
+        }
+        return null;
     }
 
     @Override
     public void deleteClienteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteClienteById'");
+        if (clienteJpaRepository.existsById(id)) {
+            clienteJpaRepository.deleteById(id);
+        }
     }
 
     @Override
     public Cliente updateClienteByCuit(String cuit, Cliente cliente) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateClienteByCuit'");
+        Cliente existingCliente = getClienteByCuit(cuit);
+        if (existingCliente != null) {
+            cliente.setId(existingCliente.getId());
+            return clienteJpaRepository.save(cliente);
+        }
+        return null;
     }
 
     @Override
     public void deleteClienteByCuit(String cuit) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteClienteByCuit'");
+        Cliente existingCliente = getClienteByCuit(cuit);
+        if (existingCliente != null) {
+            clienteJpaRepository.delete(existingCliente);
+        }
     }
-    
 }
